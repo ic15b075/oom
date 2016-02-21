13e99c5af6dbdff4bd7e49705ec12544aaa8b98c
ic15b075 <ic15b075@technikum-wien.at>
2016-02-21 10:57:14 +0100

    add project Task2

---
+++ tasks/Task2/Task2/Car.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Car
+    {
+        private Boolean fuel; // true = Benzin, false = Diesel
+        private String color; // Farbe des Autos
+        private int tank; //Größe des Tanks
+
+        public const decimal diesel = 1.1M;
+        public const decimal super = 1.5M;
+
+
+        public Car(Boolean fuel, String color,int tank)
+        {
+            this.Fuel = fuel;
+            this.Color = color;
+            this.Tank = tank;  
+        }
+
+        public Boolean Fuel
+        {
+            get
+            {
+                return this.fuel;
+            }
+            set
+            {
+                this.fuel = value;
+            }
+        }
+
+        public String Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+
+        public int Tank
+        {
+
+            get
+            {
+                return this.tank;
+            }
+            set
+            {
+                if (value <= 0) throw new Exception(" Tankgröße kann nicht negativ sein");
+
+                this.tank = value;
+            }
+
+        }
+
+        public void ChangeTank(int tank)
+        {
+            if ((tank <=0)|| (tank>100))
+                {
+                throw new ArgumentException("Tankgrösse muss zwischen 0 und 100 liegen");
+                }
+            else
+            {
+                this.tank = tank;
+            }
+                 
+
+        }
+
+        public decimal FillUp(Boolean fuel,int tank)
+        {
+            decimal sum;
+
+            if (fuel ==true)
+            {
+              sum=  super * tank;
+                return sum;
+
+            }
+            else
+            {
+                sum = diesel * tank;
+                return sum;
+            }
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car a = new Car(true, "blue", 50);
+            Car b = new Car(false, "red", 40);
+            Car c = new Car(false, "red", 20);
+
+            Console.WriteLine("Tankgrösse Auto a:"+ a.Tank);
+            Console.WriteLine("Farbe von Auto a:"+ a.Color);
+            Console.WriteLine("Trebstoffart von Auto b:" + b.Fuel);
+            Console.WriteLine("Farbe von Auto b:"+ b.Color);
+
+            Console.WriteLine("Farbe von Auto c:"+ (c.Color = "yellow"));
+            Console.WriteLine("tanken von Auto a, Kosten:"+ a.FillUp(true,50));
+
+            Console.WriteLine("Tankgrösse Auto c:" + c.Tank);
+            c.ChangeTank(55);
+            Console.WriteLine("Neue Tankgrösse Auto c:" + c.Tank);
+
+        }
+    }
+}
